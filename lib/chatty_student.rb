6a44a766a26bad18552cbbdb4e02fdc187feb0e3
Ayana Bando <ayanaflax@yahoo.com>
2018-05-11 17:03:50 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -2,9 +2,7 @@ class ChattyStudent < Student
   def hello
     super
     @phrase = phrase
-    phrase = "How are you doing today? I'm okay, but I'm kind of tired. Did you
-    watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What,
-    you don't want any spoilers? Okay well let me just tell you who died..."
+    phrase = "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
     puts phrase
   end
 
