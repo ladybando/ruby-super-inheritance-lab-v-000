fa60f837d6532046925802ea9343d53c0b27a43b
Ayana Bando <ayanaflax@yahoo.com>
2018-05-11 17:15:46 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,11 +1,18 @@
 class ChattyStudent < Student
   def hello
-    super + "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+    super
+    @phrase = phrase
+    phrase = "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+    puts phrase
   end
 
   def raise_hand
-    super + "Pick me!"
-
+    super
+    @phrase = phrase
+    phrase ="Pick me!"
+    10.times{
+      puts phrase
+    }
   end
 
 end
